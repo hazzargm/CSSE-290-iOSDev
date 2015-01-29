87a8452e587a1cc66db144ef13602dab26d5f078
Gordon Hazzard <hazzargm@rose-hulman.edu>
2015-01-28 21:37:02 -0500

    Initial commit of just a Tab-Based App Template for the Project.

---
+++ GasStats/GasStats/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GasStats
+//
+//  Created by CSSE Department on 1/28/15.
+//  Copyright (c) 2015 Gordon Hazzard & Grant Smith. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
