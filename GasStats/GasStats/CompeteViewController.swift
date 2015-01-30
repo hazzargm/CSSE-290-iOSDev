e56dc951938aeca4b63dc69de527276dff38d6cc
smithgb <g_rant@grants-mbp.wlan.rose-hulman.edu>
2015-01-30 16:05:45 -0500

    ui changes

---
+++ GasStats/GasStats/CompeteViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CompeteViewController.swift
+//  GasStats
+//
+//  Created by Grant Smith on 01/30/15.
+//  Copyright (c) 2015 Gordon Hazzard & Grant Smith. All rights reserved.
+//
+
+import UIKit
+
+class CompeteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GasStats/GasStats/GarageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  GasStats
+//
+//  Created by Grant Smith on 01/30/15.
+//  Copyright (c) 2015 Gordon Hazzard & Grant Smith. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
