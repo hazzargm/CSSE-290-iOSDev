767b0df30c4f895e0f765133c860f71328e0416c
Gordon Hazzard <hazzargm@rose-hulman.edu>
2015-02-15 23:40:25 -0500

    tank records update on fuel up

--- GasStats/GasStats/FuelUpViewController.swift
+++ GasStats/GasStats/FuelUpViewController.swift
@@ -29,11 +29,15 @@ class FuelUpViewController: SuperViewController, UIPickerViewDelegate, UIPickerV
     @IBOutlet weak var carPicker: UIPickerView!
 	
     var cars = [GTLGasstatsCar]()
+    var tankRecord = GTLGasstatsTankRecord()
 	var _refreshControl : UIRefreshControl?
 	var mpg: Double = 0.0
+    var statCount: NSNumber = 0
 	
 	override func viewWillAppear(animated: Bool) {
 		_queryForCars()
+        _queryForGasstats(NSNumber.init(integer: 0), userId: self.user_id)
+        _queryForTankRecord(NSNumber.init(integer: 0), userID: self.user_id)
 	}
 	
     override func viewDidLoad() {
@@ -43,6 +47,8 @@ class FuelUpViewController: SuperViewController, UIPickerViewDelegate, UIPickerV
     }
     
     @IBAction func pressedLogIt(sender: AnyObject){
+        _queryForGasstats(NSNumber.init(integer: carPicker.selectedRowInComponent(0)), userId: self.user_id)
+        _queryForTankRecord(NSNumber.init(integer: carPicker.selectedRowInComponent(0)), userID: tankRecord.userId)
         var miles = (milesTextField.text as NSString).floatValue
         var gallons = (gallonsTextField.text as NSString).floatValue
         var cost = (costTextField.text as NSString).floatValue
@@ -55,6 +61,7 @@ class FuelUpViewController: SuperViewController, UIPickerViewDelegate, UIPickerV
         gasStat.userId = user_id
         gasStat.carId = carPicker.selectedRowInComponent(0)
         _insertGasStat(gasStat)
+        self.updateTankRecord()
         updateButtonsAndRecords(false)
     }
     
@@ -113,6 +120,75 @@ class FuelUpViewController: SuperViewController, UIPickerViewDelegate, UIPickerV
             newGasStat.entityKey = returnedGasStat.entityKey
         })
     }
+    
+    func _queryForTankRecord(carID: NSNumber, userID: NSNumber) {
+        let query = GTLQueryGasstats.queryForTankrecordListByCarUser() as GTLQueryGasstats
+        query.limit = 1
+        query.userId = userID.longLongValue
+        query.carId = carID.longLongValue
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        service.executeQuery(query, completionHandler: { (ticket, response, error) -> Void in
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+            self.initialQueryComplete = true
+            self._refreshControl?.endRefreshing()
+            if error != nil {
+                self._showErrorDialog(error!)
+            } else {
+                let recordCollection = response as GTLGasstatsTankRecordCollection
+                if recordCollection.items() != nil {
+                    var records = recordCollection.items() as [GTLGasstatsTankRecord]
+                    self.tankRecord = records[0]
+                } else {
+                    self.tankRecord = GTLGasstatsTankRecord()
+                    self.tankRecord.avgTank = 0
+                    self.tankRecord.bestTank = 0
+                    self.tankRecord.lastTank = 0
+                    self.tankRecord.carId = carID
+                    self.tankRecord.userId = userID
+                }
+            }
+        })
+    }
+    
+    func _insertTankRecord(record: GTLGasstatsTankRecord) {
+        let query = GTLQueryGasstats.queryForTankrecordInsertWithObject(record) as GTLQueryGasstats
+        if isLocalHostTesting {
+            query.JSON = record.JSON
+            query.bodyObject = nil
+        }
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        service.executeQuery(query, completionHandler: { (ticket, response, error) -> Void in
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+            if error != nil {
+                self._showErrorDialog(error!)
+                return
+            }
+            let returnedRecord = response as GTLGasstatsTankRecord
+            record.entityKey = returnedRecord.entityKey
+        })
+    }
+    
+    func _queryForGasstats(carId : NSNumber, userId : NSNumber) {
+        let query = GTLQueryGasstats.queryForGasstatListByCarUser() as GTLQueryGasstats
+        query.userId = userId.longLongValue
+        query.carId = carId.longLongValue
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        service.executeQuery(query, completionHandler: { (ticket, response, error) -> Void in
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+            self.initialQueryComplete = true
+            self._refreshControl?.endRefreshing()
+            if error != nil {
+                self._showErrorDialog(error!)
+            } else {
+                let statCollection = response as GTLGasstatsGasStatCollection
+                var stats = [GTLGasstatsGasStat]()
+                if statCollection.items() != nil{
+                    stats = statCollection.items() as [GTLGasstatsGasStat]
+                    self.statCount = NSNumber.init(integer: stats.count)
+                }
+            }
+        })
+    }
 	
 	// MARK: - Picker View Methods
 	func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
@@ -120,7 +196,9 @@ class FuelUpViewController: SuperViewController, UIPickerViewDelegate, UIPickerV
 	}
 	
 	func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-		// TODO: set current car to car at position 'row' in array of user's cars
+        updateButtonsAndRecords(true)
+        _queryForGasstats(NSNumber.init(integer: row), userId: self.user_id)
+        _queryForTankRecord(NSNumber.init(integer: row), userID: self.user_id)
 	}
 	
 	func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String!{
@@ -150,15 +228,13 @@ class FuelUpViewController: SuperViewController, UIPickerViewDelegate, UIPickerV
 			gallons = (gallonsTextField.text as NSString).doubleValue
 		}
 		
-		if (miles == 0){
+		if (miles == 0 || gallons == 0){
 			mpg = 0.0
-		}else if(gallons == 0){
-			mpg = 99.9
-		}else{
+            mpgLabel.text = String(format: "%.2f MPG", mpg)
+		} else {
 			mpg = miles / gallons
+            mpgLabel.text = String(format: "%.2f MPG", mpg)
 		}
-		
-		mpgLabel.text = String(format: "%.2f MPG", mpg)
     }
     
     func updateButtonsAndRecords(editing : Bool) {
@@ -167,6 +243,23 @@ class FuelUpViewController: SuperViewController, UIPickerViewDelegate, UIPickerV
         self.topLabelsView.opaque = editing
         self.bottomLabelsView.hidden = editing
         self.bottomLabelsView.opaque = editing
+    }
+    
+    func updateTankRecord() {
+        tankRecord.lastTank = NSNumber.init(double: mpg)
+        if (tankRecord.bestTank.doubleValue < mpg)
+        {
+            tankRecord.bestTank = NSNumber.init(double: mpg)
+        }
+        var total = statCount.doubleValue * tankRecord.avgTank.doubleValue
+        total = total + mpg
+        var newAvg = total / (statCount.doubleValue + 1)
+        tankRecord.avgTank = NSNumber.init(double: newAvg)
+        bestTankLabel.text! = NSString(format: "Best Tank: %.2f", tankRecord.bestTank.doubleValue)
+        avgTankLabel.text! = NSString(format: "Avg. Tank: %.2f", tankRecord.avgTank.doubleValue)
+        lastTankLabel.text! = NSString(format: "last Tank: %.2f", tankRecord.lastTank.doubleValue)
+        gasBillLabel.text! = NSString(format: "Gas Bill: $%.2f", 99.99)
+        _insertTankRecord(tankRecord)
     }
 
 }
